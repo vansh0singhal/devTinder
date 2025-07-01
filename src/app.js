ed91e5cbdfb85d42a5f64ecc9217cd585bc0feab
vansh0singhal <vanshsinghal101@gmail.com>
2025-07-01 18:29:57 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
 const express=require("express");
 
 const app=express();
-
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the hello server");
+app.get("/user",(req,res)=>{
+    res.send({firstname:"vansh",lastname:"singhal"});
 
 });
+
+app.post("/user",(req,res)=>{
+    console.log("data save to db");
+    res.send("data succesfully save to db");
+
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("delete data form db");
+
+});
+
+
 
 app.use("/test",(req,res)=>{
     res.send("Hello from the test server");
-
-});
-
-app.use("/",(req,res)=>{
-    res.send("Hello main from the dashboard");
 
 });
 

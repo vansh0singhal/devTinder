8f7afaab6f7517d35cea96c3d7c2caf2ca78a64c
vansh0singhal <vanshsinghal101@gmail.com>
2025-07-02 23:50:41 +0530

    middlewares

--- src/app.js
+++ src/app.js
@@ -2,24 +2,28 @@ const express=require("express");
 
 const app=express();
 
-app.use("/user",
+// get =>/user==> middleware chain ==> request handler
+app.use("/",(req,res,next)=>{
+    //res.send("hello from the /");
+    next();
+
+});
+
+app.get("/user",
     (req,res,next)=>{
-        console.log("handling the user route 1");
         next();
+
     },
     (req,res,next)=>{
-        console.log("handling the user route 2");
-        next();
+        res.send("helo helo helo");
+
     },
     (req,res,next)=>{
-        console.log("handling the user route 3");
-        next();
+        res.send("heelo from the user");
+
     },
-    (req,res,next)=>{
-        console.log("handling the user route 4");
-        next();
-    }
-);
+
+)
 
 app.listen(3000,()=>{
     console.log("Server is listening on the port 3000");

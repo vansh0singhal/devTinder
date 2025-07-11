8bebd3894e34da16c71abbc386deac17e50f70b9
vansh0singhal <vanshsinghal101@gmail.com>
2025-07-12 00:30:47 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -5,9 +5,30 @@ const app=express();
 
 app.use(express.json());
 
+app.get("/user",async (req,res)=>{
+    const userEmail=req.body.emailId;
+    try{
+        const users=await User.findOne({emailId:userEmail});
+        
+        if(!users){
+            res.status(404).send("User not found");
+        }else{
+            res.send(users);
+        }
+        // const users=await User.find({emailId:userEmail});
+        // if(users.length===0){
+        //     res.status(404).send("User not found");
+
+        // }else{
+        //     res.send(users);
+        // }
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+});
+
 app.post("/signup",async (req,res)=>{
-    //console.log(req.body);
-    //creating the new instance of the user model
     const user=new User(req.body);
     try{
         await user.save();
@@ -16,6 +37,16 @@ app.post("/signup",async (req,res)=>{
         res.status(400).send("error while adding user"+ err.message);
     } 
 
+});
+
+//Feed API -get all the users form the database
+app.get("/feed",async (req,res)=>{
+    try{
+        const users=await User.find({});
+        res.send(users);
+    }catch(err){
+        res.status(400).send("something went wrong");
+    }
 });
 
 

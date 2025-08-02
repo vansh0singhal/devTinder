1f291e89606e9053d2c8d51be65a0918b20ff5f2
vansh0singhal <vanshsinghal101@gmail.com>
2025-08-02 12:27:42 +0530

    FEED-API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express=require("express");
 const userRouter=express.Router();
 const {userAuth}=require("../middlewares/auth.js");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user.js");
 
 const USER_SAFE_DATA="firstName lastName age gender about skills photoUrl";
 
@@ -28,6 +29,8 @@ userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
 });
 
 userRouter.get("/user/connections",userAuth,async(req,res)=>{
+    try{
+
     const loggedInUser=req.user;
 
     const connectionRequest=await ConnectionRequest.find({
@@ -47,6 +50,57 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
 });
 
     res.json({data});
+
+    }catch(err){
+        res.status(400).send("Error :"+err+message);
+    }
+    
+
+});
+
+userRouter.get("/feed",userAuth,async(req,res)=>{
+    try{
+        //user should see all the cards except
+        //1->his own card
+        //2->his connections
+        //3->ignored people
+        //4->already sent the connection request
+
+        const loggedInUser=req.user;
+
+        const page=parseInt(req.query.page) || 1;
+        let limit=parseInt(req.query.limit)||10;
+        limit= limit>50 ? 50 : limit;
+
+
+
+        const skip=(page-1)*limit;
+
+
+        const connectionRequest=await ConnectionRequest.find({
+            $or:[ {fromUserId:loggedInUser._id},{toUserId:loggedInUser._id}],
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed=new Set();
+
+        connectionRequest.forEach((req)=>{
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+        
+        const users=await User.find({
+            $and:[
+                {_id: {$nin: Array.from(hideUsersFromFeed)}},
+                {_id: {$ne:loggedInUser._id}},
+            ],
+        }).select(USER_SAFE_DATA).skip(skip).limit(limit);
+
+        res.json({data:users});
+
+    }catch(err){
+        res.status(400).json({message:err.message});
+    }
+
 
 });
 
